f5922ce643e8af3871361f2c360a4bdc311a47f9
minsu0806 <minsu0806@gmail.com>
2023-04-02 23:53:10 +0900

    정리

--- search/binary_search_test.go
+++ search/binary_search_test.go
@@ -10,9 +10,11 @@ import (
 
 func TestBinarySearch(t *testing.T) {
 	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10} // 배열은 정렬이 되어 있는 것으로 간주
+
 	idx, err := binarySearch(arr, 3, 0, len(arr)-1)
 	require.NoError(t, err)
 	require.Equal(t, 2, idx)
+
 	idx, err = binarySearch(arr, 8, 0, len(arr)-1)
 	require.NoError(t, err)
 	require.Equal(t, 7, idx)
